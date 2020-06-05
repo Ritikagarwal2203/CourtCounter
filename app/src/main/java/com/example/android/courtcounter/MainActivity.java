4010582a2f4d8c1e87946cf9d8eafcdde9f5534e
Ritik <ritik9861362764@gmail.com>
2020-06-05 10:04:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.android.courtcounter;
+
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    int scoreTeamA=0;
+    int scoreTeamB=0;
+    /**
+     * Increase the score for Team A by 1 point.
+     */
+    public  void resetScore(View v){
+        scoreTeamA=0;
+        scoreTeamB=0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
+    }
+    public void addOneForTeamA(View v) {
+        scoreTeamA=scoreTeamA+1;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Increase the score for Team A by 2 points.
+     */
+    public void addTwoForTeamA(View v) {
+        scoreTeamA=scoreTeamA+2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Increase the score for Team A by 3 points.
+     */
+    public void addThreeForTeamA(View v) {
+        scoreTeamA=scoreTeamA+3;
+        displayForTeamA(scoreTeamA);
+    }
+    public void addOneForTeamB(View v) {
+        scoreTeamB=scoreTeamB+1;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Increase the score for Team A by 2 points.
+     */
+    public void addTwoForTeamB(View v) {
+        scoreTeamB=scoreTeamB+2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Increase the score for Team A by 3 points.
+     */
+    public void addThreeForTeamB(View v) {
+        scoreTeamB=scoreTeamB+3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+}
